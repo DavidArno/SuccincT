86b0d0d1083f0a97bb5fb1e064b2301b698066a4
David <darno@interica.com>
2013-12-03 22:34:51 +0000

    Added the ValueOrError class, which is a special option class that encapsulates two string states: a string value and a string error. Supports functional pattern matching on the value/error state.

---
+++ SuccincT/Options/ValueOrError.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SuccincT.Options
+{
+    public class ValueOrError
+    {
+        private readonly string _value;
+        private readonly string _error;
+
+        private ValueOrError(string value, string error)
+        {
+            _value = value;
+            _error = error;
+        }
+
+        public static ValueOrError CreateWithValue(string value)
+        {
+            return new ValueOrError(value, null);
+        }
+
+        public static ValueOrError CreateWithError(string error)
+        {
+            return new ValueOrError(null, error);
+        }
+
+        public void MatchAndAction(Action<string> valueAction, Action<string> errorAction)
+        {
+            if (_value != null)
+            {
+                valueAction(_value);
+            }
+            else
+            {
+                errorAction(_error);
+            }
+        }
+
+        public T MatchAndResult<T>(Func<string, T> valueFunction, Func<string, T> errorFunction) 
+        {
+            return _value != null ? valueFunction(_value) : errorFunction(_error);
+        }
+    }
+}
---
+++ SuccincTTests/SuccincT/Options/ValueOrErrorTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using SuccincT.Options;
+
+namespace SuccincTTests.SuccincT.Options
+{
+    [TestFixture]
+    public class ValueOrErrorTests
+    {
+        [Test]
+        public void WhenValueIsSet_OnlyValueActionOccurs()
+        {
+            var valueOrError = ValueOrError.CreateWithValue("x");
+            var valueSet = false;
+            var errorSet = false;
+            valueOrError.MatchAndAction(s => { valueSet = true; }, s => { errorSet = true; });
+            Assert.AreEqual(new[] { true, false }, new[] { valueSet, errorSet });
+        }
+
+        [Test]
+        public void WhenErrorIsSet_OnlyErrorActionOccurs()
+        {
+            var valueOrError = ValueOrError.CreateWithError("x");
+            var valueSet = false;
+            var errorSet = false;
+            valueOrError.MatchAndAction(s => { valueSet = true; }, s => { errorSet = true; });
+            Assert.AreEqual(new[] { false, true }, new[] { valueSet, errorSet });
+        }
+
+        [Test]
+        public void WhenValueIsSet_ValueSuppliedToAction()
+        {
+            var valueOrError = ValueOrError.CreateWithValue("x");
+            var value = string.Empty;
+            valueOrError.MatchAndAction(s => { value = s; }, s => { });
+            Assert.AreEqual("x", value);
+        }
+
+        [Test]
+        public void WhenErrorIsSet_ErrorSuppliedToAction()
+        {
+            var valueOrError = ValueOrError.CreateWithError("x");
+            var errorValue = string.Empty;
+            valueOrError.MatchAndAction(s => { }, s => { errorValue = s; });
+            Assert.AreEqual("x", errorValue);
+        }
+
+        [Test]
+        public void WhenValueIsSet_ValueSuppliedToFunction()
+        {
+            var valueOrError = ValueOrError.CreateWithValue("1");
+            var result = valueOrError.MatchAndResult(s => "v" + s, s => "e" + s);
+            Assert.AreEqual("v1", result);
+        }
+
+        [Test]
+        public void WhenErrorIsSet_ErrorSuppliedToFunction()
+        {
+            var valueOrError = ValueOrError.CreateWithError("2");
+            var result = valueOrError.MatchAndResult(s => "v" + s, s => "e" + s);
+            Assert.AreEqual("e2", result);
+        }
+    }
+}
