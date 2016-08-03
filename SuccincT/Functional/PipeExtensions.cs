41b81b7ba51f1f16ee0a042d622cf33901628ea9
David Arno <david@davidarno.org>
2016-08-03 05:41:14 +0100

    Experimental forward pipe extension method added.

---
+++ SuccincT/Functional/PipeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SuccincT.Functional
+{
+    public static class PipeExtensions
+    {
+        public static TR PipeTo<T, TR>(this T value, Func<T, TR > func) => func(value);
+    }
+}
--- SuccincTTests/SuccincT/Functional/ForwardPipeTests.cs
+++ SuccincTTests/SuccincT/Functional/ForwardPipeTests.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 using ReversedString;
+using SuccincT.Functional;
 using static SuccincT.Functional.TypedLambdas;
-
 
 namespace SuccincTTests.SuccincT.Functional
 {
@@ -9,12 +9,28 @@ namespace SuccincTTests.SuccincT.Functional
     public class ForwardPipeTests
     {
         [Test]
-        public void OneParameterPipe_GivesSameResultAsStandardWay()
+        public void OneParameterPipeWithLambdas_GivesSameResultAsStandardWay()
         {
-            var square = Lambda<int>(x => x*x);
+            var square = Lambda<int>(x => x * x);
             var toStr = Transform((int x) => x.ToString());
             var rev = Lambda<string>(x => x.Reverse());
-            var result = rev(toStr(square(512)));
+            var expected = rev(toStr(square(512)));
+
+            var actual = 512.PipeTo(square).PipeTo(toStr).PipeTo(rev);
+
+            Assert.AreEqual(expected, actual);
         }
+        [Test]
+        public void OneParameterPipeWithMethods_GivesSameResultAsStandardWay()
+        {
+            var expected = ToString(Square(512)).Reverse();
+
+            var actual = 512.PipeTo(Square).PipeTo(ToString).PipeTo(StringReverseExtension.Reverse);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static int Square(int x) => x * x;
+        private static string ToString(int x) => x.ToString();
     }
 }
