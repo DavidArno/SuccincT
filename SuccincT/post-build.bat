d5d613443eb96a9618236b7ee50037bdd4ea77a8
David Arno <david@davidarno.org>
2017-01-03 19:06:48 +0000

    Tidied up version that now compiles on my machine and on appveyor.

--- SuccincT/post-build.bat
+++
@@ -1,2 +0,0 @@
-for %%f in (Functional Options Parsers PatternMatchers Tuples Unions) do if not exist %1\..\SuccincT.Core\%%f mklink /D  %1\..\SuccincT.Core\%%f %1\%%f
-for %%f in (CompilationTargetHandler.cs Version.cs) do if not exist %1\..\SuccincT.Core\%%f mklink %1\..\SuccincT.Core\%%f %1\%%f
