55e4036db194e260b9eb428e71e5a4a12c6bc08d
David Arno <david@davidarno.org>
2017-01-04 08:25:55 +0000

    Added missing tests for indexed enumerations.

--- SuccincTTests/SuccincT/Functional/IEnumerableExtensionsTests.cs
+++ SuccincTTests/SuccincT/Functional/IEnumerableExtensionsTests.cs
@@ -2,6 +2,7 @@
 using SuccincT.Functional;
 using System.Collections.Generic;
 using System.Linq;
+using static NUnit.Framework.Assert;
 
 namespace SuccincTTests.SuccincT.Functional
 {
@@ -13,7 +14,35 @@ namespace SuccincTTests.SuccincT.Functional
         {
             var emptyList = new List<string>();
             var indexedList = emptyList.Indexed().ToList();
-            Assert.AreEqual(0, indexedList.Count);
+            AreEqual(0, indexedList.Count);
+        }
+
+        [Test]
+        public void ForList_IndexedEnumerationReturned()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            var indexedList = list.Indexed();
+            foreach (var indexedItem in indexedList)
+            {
+                AreEqual(indexedItem.index + 1, indexedItem.item);
+            }
+        }
+
+        [Test]
+        public void ForEnumeration_IndexedEnumerationReturned()
+        {
+            var indexedEnumeration = StringEnumeration().Indexed();
+            foreach (var indexedItem in indexedEnumeration)
+            {
+                AreEqual($"{indexedItem.index}", indexedItem.item);
+            }
+        }
+
+        private IEnumerable<string> StringEnumeration()
+        {
+            yield return "0";
+            yield return "1";
+            yield return "2";
         }
     }
 }
