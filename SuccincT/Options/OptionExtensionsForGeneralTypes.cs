c2457f3983ce3689e9c938a09dd8fec289cd19cf
David Arno <david@davidarno.org>
2017-01-27 14:13:49 +0000

    Modified some of the new features methods from @megafinz to be expression body methods.

--- SuccincT/Options/OptionExtensionsForGeneralTypes.cs
+++ SuccincT/Options/OptionExtensionsForGeneralTypes.cs
@@ -4,14 +4,10 @@ namespace SuccincT.Options
 {
     public static class OptionExtensionsForGeneralTypes
     {
-        public static Option<T> ToOption<T>(this T obj)
-        {
-            return obj != null ? obj.Into(Option<T>.Some) : Option<T>.None();
-        }
+        public static Option<T> ToOption<T>(this T obj) => 
+            obj != null ? obj.Into(Option<T>.Some) : Option<T>.None();
 
-        public static T? AsNullable<T>(this Option<T> option) where T : struct
-        {
-            return option.HasValue ? option.Value : (T?) null;
-        }
+        public static T? AsNullable<T>(this Option<T> option) where T : struct => 
+            option.HasValue ? option.Value : (T?) null;
     }
 }
