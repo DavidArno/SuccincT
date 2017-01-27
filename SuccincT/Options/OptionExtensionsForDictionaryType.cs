0d6335ef61c10a551857a204fba0a6a679d5721b
Tony Sedniov <tony.sedniov@alt-soft.com>
2017-01-27 15:59:36 +0300

    TryGetValue now works for any IDictionary<,> type, not just Dictionary<,>.

--- SuccincT/Options/OptionExtensionsForDictionaryType.cs
+++ SuccincT/Options/OptionExtensionsForDictionaryType.cs
@@ -4,7 +4,7 @@ namespace SuccincT.Options
 {
     public static class OptionExtensionsForDictionaryType
     {
-        public static Option<TValue> TryGetValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+        public static Option<TValue> TryGetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
             TValue value;
             return dictionary.TryGetValue(key, out value) ? Option<TValue>.Some(value) : Option<TValue>.None();
