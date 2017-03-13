9d7d694540ec2b9d3852e17f29bf699ac905a769
Tony Sedniov <tony.sedniov@alt-soft.com>
2017-03-14 02:23:26 +0300

    Improved Union<T1, T2> creation performance (10 times less allocations, 20+ times less CPU time).

---
+++ SuccincTPerformanceTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SuccincT.Unions;
+
+namespace SuccincTPerformanceTests
+{
+    [MemoryDiagnoser]
+    public class UnionT1T2Benchmarks
+    {
+        private const int Count = 1_000;
+
+        [Benchmark]
+        public void UnionT1T2Benchmark()
+        {
+            var unions = new List<Union<int, string>>();
+
+            for (var i = 0; i < Count; ++i)
+            {
+                unions.Add(new Union<int, string>(1));
+            }
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var results = BenchmarkRunner.Run<UnionT1T2Benchmarks>();
+            Console.WriteLine(results.ToString());
+        }
+    }
+}
