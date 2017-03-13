6f54ed0726ef9cff755adb6bb74ea0100dc57760
Tony Sedniov <tony.sedniov@alt-soft.com>
2017-03-13 22:27:28 +0300

    Added Into overload that accepts an Action.

--- SuccincTTests/SuccincT/Functional/ForwardPipeTests.cs
+++ SuccincTTests/SuccincT/Functional/ForwardPipeTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using ReversedString;
 using SuccincT.Functional;
 using static SuccincT.Functional.TypedLambdas;
@@ -29,6 +30,31 @@ namespace SuccincTTests.SuccincT.Functional
             var actual = 512.Into(Square).ToString().Into(rev);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OneActionParameterPipeWithLambdas_GivesSameResultAsStandardWay()
+        {
+            var list1 = new List<int> { 1, 2, 3 };
+            var list2 = new List<int> { 1, 2, 3 };
+            var removeFromList1 = Action<int>(x => list1.Remove(x));
+
+            2.Into(removeFromList1);
+            list2.Remove(2);
+
+            CollectionAssert.AreEqual(list1, list2);
+        }
+
+        [Test]
+        public void OneActionParameterPipeWithMethods_GivesSameResultAsStandardWay()
+        {
+            var list1 = new List<int> { 1, 2, 3 };
+            var list2 = new List<int> { 1, 2, 3 };
+
+            2.Into(list1.Remove);
+            list2.Remove(2);
+
+            CollectionAssert.AreEqual(list1, list2);
         }
 
         private static int Square(int x) => x * x;
