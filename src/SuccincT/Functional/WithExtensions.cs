3975487f49434f21530826071494a3cd803d6fc6
Odonno <bottiau.david@laposte.net>
2017-11-24 15:54:52 +0100

    feat(Functional): add With extension method

---
+++ src/SuccincT/Functional/WithExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace SuccincT.Functional
+{
+    public static class WithExtensions
+    {
+        public static T Copy<T>(this T @object) where T : new()
+        {
+            var newObject = new T();
+
+            var sourceProps = typeof(T).GetRuntimeProperties()
+                .Where(x => x.CanRead)
+                .ToList();
+
+            var destProps = typeof(T).GetRuntimeProperties()
+                .Where(x => x.CanWrite)
+                .ToList();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                if (destProps.Any(x => x.Name == sourceProp.Name))
+                {
+                    var p = destProps.First(x => x.Name == sourceProp.Name);
+                    if (p.CanWrite)
+                    { 
+                        p.SetValue(newObject, sourceProp.GetValue(@object, null), null);
+                    }
+                }
+            }
+
+            return newObject;
+        }
+
+        public static T With<T, TProps>(this T @object, TProps propertiesToUpdate) where T : new() where TProps : class
+        {
+            if (propertiesToUpdate == null)
+                return @object;
+
+            var newObject = Copy(@object);
+
+            var sourceProps = typeof(T).GetRuntimeProperties()
+                .Where(x => x.CanRead)
+                .ToList();
+            var propsToUpdate = typeof(TProps).GetRuntimeProperties()
+                .Where(x => x.CanRead)
+                .ToList();
+
+            foreach (var propToUpdate in propsToUpdate)
+            {
+                var updatingProp = sourceProps.FirstOrDefault(sp => sp.Name == propToUpdate.Name);
+
+                if (updatingProp != null && updatingProp.CanWrite)
+                {
+                    updatingProp.SetValue(newObject, propToUpdate.GetValue(propertiesToUpdate, null), null);
+                }
+            }
+
+            return newObject;
+        }
+    }
+}
---
+++ tests/SuccincT.Tests/SuccincT/Functional/WithExtensionsTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using NUnit.Framework;
+using SuccincT.Functional;
+using static NUnit.Framework.Assert;
+
+namespace SuccincTTests.SuccincT.Functional
+{
+    [TestFixture]
+    public class WithExtensionsTests
+    {
+        [Test]
+        public void UpdatingWithNull_ShouldReturnSameObject()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var newCar = car.With<Car, Car>(null);
+
+            // Assert
+            AreSame(car, newCar);
+        }
+
+        [Test]
+        public void UpdatingWithoutProperty_ShouldReturnImmutableObject()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var newCar = car.With(new { });
+
+            // Assert
+            AreNotSame(car, newCar);
+            AreEqual(car.Constructor, newCar.Constructor);
+            AreEqual(car.Color, newCar.Color);
+            AreEqual(car.CreationDate, newCar.CreationDate);
+        }
+
+        [Test]
+        public void UpdatingWithOneProperty_ShouldReturnImmutableObjectWithUpdatedProperties()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var newCar = car.With(new { Color = "Red" });
+
+            // Assert
+            AreNotSame(car, newCar);
+            AreEqual(car.Constructor, newCar.Constructor);
+            AreEqual("Red", newCar.Color);
+            AreEqual(car.CreationDate, newCar.CreationDate);
+        }
+
+        [Test]
+        public void UpdatingWithOnePropertyMultipleTimes_ShouldReturnImmutableObjectWithLastUpdatedProperties()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var newCar1 = car.With(new { Color = "Red" });
+            var newCar2 = newCar1.With(new { Color = "Blue" });
+            var newCar3 = newCar2.With(new { Color = "Green" });
+
+            // Assert
+            AreNotSame(car, newCar3);
+            AreEqual(car.Constructor, newCar3.Constructor);
+            AreEqual("Green", newCar3.Color);
+            AreEqual(car.CreationDate, newCar3.CreationDate);
+        }
+
+        [Test]
+        public void UpdatingWithAllProperties_ShouldReturnImmutableObjectWithUpdatedProperties()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var newCar = car.With(new
+            {
+                Constructor = "BMW",
+                Color = "Red",
+                CreationDate = new DateTime(2017, 01, 01)
+            });
+
+            // Assert
+            AreNotSame(car, newCar);
+            AreEqual("BMW", newCar.Constructor);
+            AreEqual("Red", newCar.Color);
+            AreEqual(new DateTime(2017, 01, 01), newCar.CreationDate);
+        }
+
+        [Test]
+        public void UpdatingWithNoMatchingProperty_ShouldReturnImmutableObject()
+        {
+            // Arrange
+            var car = new Car { Color = "Blue" };
+
+            // Act
+            var newCar = car.With(new { Cost = "1,000$" });
+
+            // Assert
+            AreNotSame(car, newCar);
+            AreEqual(car.Constructor, newCar.Constructor);
+            AreEqual("Blue", newCar.Color);
+            AreEqual(car.CreationDate, newCar.CreationDate);
+        }
+
+        private class Car
+        {
+            public string Constructor { get; set; }
+            public string Color { get; set; }
+            public DateTime? CreationDate { get; set; }
+        }
+    }
+}
